d449e5b7980c5dab36606c77510517f3eb16a2cf
tfermoso <tomas.fermoso@gmail.com>
2023-03-31 16:17:18 +0200

    LoginServlet

---
+++ src/main/java/com/formacom/biblioteca/Controladores/LoginServlet.java
@@ -0,0 +1,20 @@
+package com.formacom.biblioteca.Controladores;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
